1de3b223c5243d79040649218f4e3f7f389a8b95
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-01-23 17:00:19 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zull/src/test/java/com/martadrozsa/hrapigatewayzull/HrApiGatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martadrozsa.hrapigatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
